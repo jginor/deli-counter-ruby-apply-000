584d3fb1eca4e879c9903ebd3ec96aa723d08e66
Jordan Ginor <jordan.ginor@gmail.com>
2018-12-17 23:22:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,52 +1,15 @@
-# Write your code here.
+katz_deli = ["Jon", "Brian", "Bob", "Bill"]
 
 def line(katz_deli)
-  line_array = []
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
     counter = 0
-    katz_deli.each do |name|
+    list = ""
+    katz_deli.each_with_index do|name, index|
       counter += 1
-      line_array << "#{counter}. #{name}"
-    end
-      puts "The line is currently: #{line_array.join(" ")}"
-  end
-end
-
-
-def line(katz_deli)
-  if katz_deli.length == 0
-      puts "The line is currently empty."
-  else
-    counter = 0
-    intro = "The line is currently:"
-    katz_deli.each do |name|
-      counter += 1
-      intro << " #{counter}. #{name}"
+      list << " #{index+1}. #{name}"
       end
-        puts intro  
+    puts "The line is currently:" << list
   end
-end
-
-katz_deli = ["Jon", "Brian", "Bob", "Bill"]
-line(katz_deli)
-
-
-def take_a_number(katz_deli, name) 
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
-
-def now_serving(katz_deli)
-  if katz_deli.length == 0 
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
-  end
-end
-  
-
-
-katz_deli = ["Jon", "Brian", "Bob", "Bill"]
+end 
